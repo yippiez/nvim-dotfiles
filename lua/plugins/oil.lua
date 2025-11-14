80d06fc7d6a9369166c0d815fdf62ffb26508cff
Eren <erencemayez@gmail.com>
2025-11-14 16:25:15 +0300

    Fixed a keymap conflict in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -12,6 +12,12 @@ return {
       "size",
     },
     use_default_keymaps = true,
+    keymaps = {
+      -- Override C-p to avoid conflict with commander
+      ["<C-p>"] = false,
+      -- Use different key for preview if needed
+      ["<C-i>"] = "actions.preview",
+    },
   },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
