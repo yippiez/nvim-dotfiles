099512ac0e7f00d112ad9d892c25855036fc6eac
Eren <erencemayez@gmail.com>
2025-12-07 01:23:58 +0300

    Matched the accept command to parital accept

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,8 @@
 -- https://github.com/zbirenbaum/copilot.lua
+-- NOTE: Accept key is Cmd+Right on macOS, Ctrl+Right on Windows/Linux
+local is_mac = vim.fn.has("macunix") == 1
+local accept_key = is_mac and "<D-Right>" or "<C-Right>"
+
 return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
@@ -9,7 +13,7 @@ return {
                 enabled = true,
                 auto_trigger = true,
                 keymap = {
-                    accept = "<Right>",
+                    accept = accept_key,
                     accept_word = false,
                     accept_line = false,
                     next = "<S-Down>",
