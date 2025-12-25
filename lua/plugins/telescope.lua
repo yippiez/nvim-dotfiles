c0f119058758f56f52d47e220002587afa9ede29
Eren <erencemayez@gmail.com>
2025-12-25 20:41:21 +0300

    perf: lazy load telescope and keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,18 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = { "<leader>f", "<C-p>" },
+    keys = {
+        { "<leader>ff", function() require("telescope.builtin").find_files() end, desc = "Telescope find files" },
+        { "<leader>fg", function() require("telescope.builtin").live_grep() end, desc = "Telescope live grep" },
+        { "<leader>fb", function() require("telescope.builtin").buffers() end, desc = "Telescope buffers" },
+        { "<leader>fh", function() require("telescope.builtin").help_tags() end, desc = "Telescope help tags" },
+        { "<leader>fc", function() require("telescope.builtin").commands() end, desc = "Telescope commands" },
+        { "<leader>fS", function() require("telescope.builtin").git_status() end, desc = "Telescope git status" },
+        { "<leader>fs", function() require("telescope.builtin").lsp_document_symbols() end, desc = "Telescope document symbols" },
+        { "<leader>fG", function() require("telescope.builtin").git_commits() end, desc = "Telescope git commits" },
+        { "<leader>fB", function() require("telescope.builtin").git_bcommits() end, desc = "Telescope buffer commits" },
+    },
+    cmd = "Telescope",
     config = function()
         require('telescope').setup({
             defaults = {
